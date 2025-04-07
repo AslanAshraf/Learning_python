55d6e5a5d0b5103cf14ab662db1086823927ea60
AslanAshraf <aslan1.devverx@gmail.com>
2025-04-07 09:38:03 -0700

    ip added

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","http://localhost:8101","http://35.154.81.229/"],
+    allow_origins=["http://localhost:5173","http://localhost:8101","http://35.154.81.229"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
